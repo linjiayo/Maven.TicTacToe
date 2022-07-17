ecfab81eaa3fdbacf6fecb696be46865255592b0
Jiayong <jiagains@gmail.com>
2022-07-17 18:55:35 -0400

    add method bodies and pass tests

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,49 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character[][] board;
     public Board(Character[][] matrix) {
+        board = matrix;
+    }
+
+    private Boolean isInFavorOfChar(Character c) {
+        int[] count = new int[8];
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board[row][col] == c) {
+                    count[row]++;
+                    count[col + 3]++;
+                    count[7] = (row + col == 2) ? ++count[7] : count[7];
+                    count[6] = (row == col) ? ++count[6]  : count[6];
+                }
+            }
+        }
+        for (int i : count) {
+            if (i == 3) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        return isInFavorOfChar('X');
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        return isInFavorOfChar('O');
     }
 
     public Boolean isTie() {
-        return null;
+        return !(isInFavorOfX() || isInFavorOfO());
     }
 
     public String getWinner() {
-        return null;
+        if (isInFavorOfX()) {
+            return "X";
+        } else if (isInFavorOfO()) {
+            return "O";
+        } else return "";
     }
 
 }
